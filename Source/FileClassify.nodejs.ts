faabfb0e15a2f636b371c9107622d8ebf2345a8c
SabineWren <sabinewrensc@gmail.com>
2026-01-27 23:28:30 -0800

    :building_construction: Shrink interface surface for file classification

--- Source/FileClassify.nodejs.ts
+++ Source/FileClassify.nodejs.ts
@@ -2,25 +2,22 @@ import path from "node:path"
 import { Array, Option, Pipe } from "../Lib/pure.ts"
 import type { Classifier } from "../Monad/pure.ts"
 
-const matchExtension = (ext: string, filename: string): boolean =>
-	filename.split(".").includes(ext)
-
-export const MatchExtension =
+const matchExtension =
 	<T extends string>(glob: T) => {
 		const exts = glob.split(".")
 
 		return (filepath: string): boolean => {
 			const filename = path.basename(filepath)
 			// glob ordering non-symmetric, ex. a.b <> b.a
-			return filename.includes(exts.join("."))
+			return filename.includes(glob)
 				// glob parts individually valid, ex. hellofoo.moo <> hello.foo.moo
-				&& exts.every(ext => matchExtension(ext, filename))
+				&& exts.every(ext => filename.split(".").includes(ext))
 		}
 	}
 
 /** Create list of filename -> extension matchers. Order matters, so put globs first. */
 export const FromExtensions = <A extends string>(exts: readonly A[]): Classifier<A> => {
-	const rules = Array.Map(exts, a => ({ Class: a, Matcher: MatchExtension(a) }))
+	const rules = Array.Map(exts, a => ({ Class: a, Matcher: matchExtension(a) }))
 
 	return filepath => Pipe(
 		Array.Find_(rules, r => r.Matcher(filepath)),
--- Source/FileClassify.test.ts
+++ Source/FileClassify.test.ts
@@ -2,125 +2,82 @@ import { describe, it } from "node:test"
 import { expect } from "expect"
 
 import { Option } from "../Lib/pure.ts"
-import type { Classifier } from "../Monad/pure.ts"
 import * as FileClassify from "./FileClassify.nodejs.ts"
 
 await describe("Classify file by one extension", async () => {
-	const hasExtension = FileClassify.MatchExtension("foo")
+	const classify = FileClassify.FromExtensions(["dom.http", "pure", "dom", "http"])
 
-	await it("Matches only extension", () => {
-		expect(hasExtension("foo.ts")).toEqual(true)
+	await it("Fails if missing", () => {
+		expect(classify("")).toEqual(Option.None())
+		expect(classify("ts")).toEqual(Option.None())
+		expect(classify(".ts")).toEqual(Option.None())
+		expect(classify("Foo.ts")).toEqual(Option.None())
+		expect(classify("Foo/Foo.ts")).toEqual(Option.None())
 	})
 
-	await it("Matches name with extension", () => {
-		expect(hasExtension("bar.foo.ts")).toEqual(true)
-	})
-
-	await it("Matches last extension", () => {
-		expect(hasExtension("bar.baz.foo.ts")).toEqual(true)
-	})
-
-	await it("Matches first extension", () => {
-		expect(hasExtension("foo.bar.baz.ts")).toEqual(true)
-	})
-
-	await it("Matches middle extension", () => {
-		expect(hasExtension("bar.foo.baz.ts")).toEqual(true)
-	})
-
-	await it("Fails if missing extension", () => {
-		expect(hasExtension("bar.moo.baz.ts")).toEqual(false)
-	})
-
-	await it("Ignores filepath", () => {
-		expect(hasExtension("hello/foo.bar.baz.ts")).toEqual(true)
-		expect(hasExtension("hello.foo/foo.bar.baz.ts")).toEqual(true)
-		expect(hasExtension("hello.foo/foo.ts")).toEqual(true)
-		expect(hasExtension("hello.foo/bar.baz.ts")).toEqual(false)
-		expect(hasExtension("foo/bar.baz.ts")).toEqual(false)
-	})
-})
-
-await describe("Classify file by multiple extensions", async () => {
-	const hasExtension = FileClassify.MatchExtension("aa.bb")
-
-	await it("Matches only provided extensions", () => {
-		expect(hasExtension("aa.bb.lua")).toEqual(true)
-	})
-
-	await it("Matches name with glob extension", () => {
-		expect(hasExtension("bar.aa.bb.lua")).toEqual(true)
-	})
-
-	await it("Matches last glob extension", () => {
-		expect(hasExtension("bar.aa.bb.lua")).toEqual(true)
-	})
-
-	await it("Matches first glob extension", () => {
-		expect(hasExtension("aa.bb.baz.lua")).toEqual(true)
-	})
-
-	await it("Matches middle glob extension", () => {
-		expect(hasExtension("bar.aa.bb.moo.lua")).toEqual(true)
-	})
-
-	await it("Fails on out-of-order glob extensions", () => {
-		expect(hasExtension("bar.aa.moo.bb.lua")).toEqual(false)
-		expect(hasExtension("bar.bb.moo.aa.lua")).toEqual(false)
-		expect(hasExtension("bar.bb.aa.moo.lua")).toEqual(false)
-	})
-
-	await it("Fails if missing all extensions", () => {
-		expect(hasExtension("bar.moo.baz.lua")).toEqual(false)
-	})
-
-	await it("Fails if missing any extensions", () => {
-		expect(hasExtension("bar.aa.baz.lua")).toEqual(false)
-		expect(hasExtension("bar.moo.bb.lua")).toEqual(false)
-	})
-
-	await it("Ignores filepath", () => {
-		expect(hasExtension("hello/bar.aa.bb.moo.lua")).toEqual(true)
-		expect(hasExtension("aa.bb/aa.bb.lua")).toEqual(true)
-		expect(hasExtension("foo.aa.bb/foo.aa.bb.lua")).toEqual(true)
-		expect(hasExtension("aa.bb/foo.lua")).toEqual(false)
-		expect(hasExtension("foo.aa.bb/foo.lua")).toEqual(false)
-	})
-})
-
-await describe("Classifier works for a few extensions", async () => {
-	const classify: Classifier<"pure" | "dom" | "dom.http" | "http"> = FileClassify
-		.FromExtensions(["dom.http", "pure", "dom", "http"])
-
-	await it("Fails with wrong extensions", () => {
-		expect(classify("foo")).toEqual(Option.None())
-		expect(classify("foo/bar")).toEqual(Option.None())
-
-		expect(classify("foo.moo")).toEqual(Option.None())
-		expect(classify("foo/bar.moo")).toEqual(Option.None())
-	})
-
-	await it("Matches extensions without path", () => {
+	await it("Matches ID", () => {
 		expect(classify("pure")).toEqual(Option.Some("pure"))
 		expect(classify("dom")).toEqual(Option.Some("dom"))
 		expect(classify("dom.http")).toEqual(Option.Some("dom.http"))
 		expect(classify("http")).toEqual(Option.Some("http"))
-
-		expect(classify("foo.pure")).toEqual(Option.Some("pure"))
-		expect(classify("foo.dom")).toEqual(Option.Some("dom"))
-		expect(classify("foo.dom.http")).toEqual(Option.Some("dom.http"))
-		expect(classify("foo.http")).toEqual(Option.Some("http"))
 	})
 
-	await it("Matches extensions with path", () => {
-		expect(classify("bar/foo.pure")).toEqual(Option.Some("pure"))
-		expect(classify("bar/foo.dom")).toEqual(Option.Some("dom"))
-		expect(classify("bar/foo.dom.http")).toEqual(Option.Some("dom.http"))
-		expect(classify("bar/foo.http")).toEqual(Option.Some("http"))
+	await it("Matches simple", () => {
+		expect(classify("pure.ts")).toEqual(Option.Some("pure"))
+		expect(classify("Bar.http.ts")).toEqual(Option.Some("http"))
+		expect(classify("foo_bar/pure.ts")).toEqual(Option.Some("pure"))
+		expect(classify("foo_bar/Bar.pure.ts")).toEqual(Option.Some("pure"))
+	})
 
-		expect(classify("bar/foo.pure")).toEqual(Option.Some("pure"))
-		expect(classify("bar/foo.dom")).toEqual(Option.Some("dom"))
-		expect(classify("bar/foo.dom.http")).toEqual(Option.Some("dom.http"))
-		expect(classify("bar/foo.http")).toEqual(Option.Some("http"))
+	await it("Matches around other extensions", () => {
+		expect(classify("Bar.http.baz.foo.ts")).toEqual(Option.Some("http"))
+		expect(classify("Bar.baz.http.foo.ts")).toEqual(Option.Some("http"))
+		expect(classify("Bar.baz.foo.http.ts")).toEqual(Option.Some("http"))
+	})
+
+	await it("Respects classification order", () => {
+		expect(classify("dom.pure.ts")).toEqual(Option.Some("pure"))
+		expect(classify("pure.dom.ts")).toEqual(Option.Some("pure"))
+		expect(classify("pure.dom.http.ts")).toEqual(Option.Some("dom.http"))
+		expect(classify("dom.http.pure.ts")).toEqual(Option.Some("dom.http"))
+	})
+
+	await it("Ignores filepath", () => {
+		expect(classify("hello/dom.bar.baz.ts")).toEqual(Option.Some("dom"))
+		expect(classify("hello.dom/dom.bar.baz.ts")).toEqual(Option.Some("dom"))
+		expect(classify("hello.dom/dom.ts")).toEqual(Option.Some("dom"))
+
+		expect(classify("hello.dom/bar.baz.ts")).toEqual(Option.None())
+		expect(classify("dom/bar.baz.ts")).toEqual(Option.None())
+	})
+})
+
+await describe("Classify file by glob extension", async () => {
+	const classify = FileClassify.FromExtensions(["worker.nodejs", "api.state", "state", "api", "pure"])
+
+	await it("Matches simple", () => {
+		expect(classify("api.state.lua")).toEqual(Option.Some("api.state"))
+		expect(classify("Foo.api.state.lua")).toEqual(Option.Some("api.state"))
+	})
+
+	await it("Matches around other extensions", () => {
+		expect(classify("api.state.aa.bb.lua")).toEqual(Option.Some("api.state"))
+		expect(classify("aa.api.state.bb.lua")).toEqual(Option.Some("api.state"))
+		expect(classify("aa.bb.api.state.lua")).toEqual(Option.Some("api.state"))
+	})
+
+	await it("Ignores filepath", () => {
+		expect(classify("Foo.pure/Foo.lua")).toEqual(Option.None())
+		expect(classify("Foo.pure/Bar.api.state.lua")).toEqual(Option.Some("api.state"))
+		expect(classify("api.state/pure.lua")).toEqual(Option.Some("pure"))
+	})
+
+	await it("Fails if out of order", () => {
+		expect(classify("Bar.worker.nodejs.aa.bb.lua")).toEqual(Option.Some("worker.nodejs"))
+		expect(classify("Bar.worker.aa.nodejs.bb.lua")).toEqual(Option.None())
+
+		expect(classify("Bar.state.api.lua")).toEqual(Option.Some("state"))
+		expect(classify("Bar.api.aa.state.bb.lua")).toEqual(Option.Some("state"))
+		expect(classify("Bar.state.aa.api.bb.lua")).toEqual(Option.Some("state"))
 	})
 })
